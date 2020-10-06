8c3d05eea0fe515b0dc8f5456d53711ed15baba1
Zairon Jacobs <zaijacobs@gmail.com>
2020-10-06 16:28:32 +0200

    small changes

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup
 from setuptools import find_packages
-from stock_position_calc.console_name import __console_name__
-from stock_position_calc.version import __version__
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+version = '1.0.4'
+
 requires = []
 
 setup(
-    name=__console_name__,
-    version=__version__,
+    name='stock-position-calc',
+    version=version,
     author='Zairon Jacobs',
     author_email='zaironjacobs@gmail.com',
     description='A simple program to calculate the right amount of shares to buy while limiting risk.',
     long_description=long_description,
     url='https://github.com/zaironjacobs/stock-position-calc',
-    download_url='https://github.com/zaironjacobs/stock-position-calc/archive/v' + __version__ + '.tar.gz',
+    download_url='https://github.com/zaironjacobs/stock-position-calc/archive/v' + version + '.tar.gz',
     keywords=['stock', 'market', 'price', 'calculation', 'investment', 'tool'],
     packages=find_packages(),
     entry_points={
-        'console_scripts': [__console_name__ + '=stock_position_calc.app:main'],
+        'console_scripts': ['stock-position-calc' + '=stock_position_calc.app:main'],
     },
     install_requires=requires,
     license='MIT',

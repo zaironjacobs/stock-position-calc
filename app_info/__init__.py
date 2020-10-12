98d608e275091cd4e4d2d307688cf32f9d30146a
Zairon Jacobs <zaijacobs@gmail.com>
2020-10-12 12:40:44 +0200

    refactor

---
+++ app_info/__init__.py
@@ -0,0 +1,2 @@
+__app_name__ = 'stock-position-calc'
+__app_version__ = '1.0.7'
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup
 from setuptools import find_packages
+from app_info import __app_name__
+from app_info import __app_version__
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-version = '1.0.6'
-
 requires = []
 
 setup(
-    name='stock-position-calc',
-    version=version,
+    name=__app_name__,
+    version=__app_version__,
     author='Zairon Jacobs',
     author_email='zaironjacobs@gmail.com',
     description='A simple program to calculate the right amount of shares to buy while limiting risk.',
     long_description=long_description,
     url='https://github.com/zaironjacobs/stock-position-calc',
-    download_url='https://github.com/zaironjacobs/stock-position-calc/archive/v' + version + '.tar.gz',
+    download_url='https://github.com/zaironjacobs/stock-position-calc/archive/v' + __app_version__ + '.tar.gz',
     keywords=['stock', 'market', 'price', 'calculation', 'investment', 'tool'],
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['stock-position-calc' + '=stock_position_calc.app:main'],
+        'console_scripts': [__app_name__ + '=stock_position_calc.app:main'],
     },
     install_requires=requires,
     license='MIT',

9eeddf28d7356af9df438d036537e293ad94eeff
Zairon Jacobs <zaijacobs@gmail.com>
2021-04-19 09:17:52 +0200

    Pipfile.lock

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 name = 'stock-position-calc'
-version = '1.0.11'
+version = '1.1'
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
